022d412623c31d2addd646b4684b3d825383e775
Cong Min <i@congm.in>
2022-03-22 12:26:46 +0800

    chore: lint

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,8 @@ const src = 'src';
 const dist = '.miniprogram';
 
 const sourcemap = {
-    ts: true, // 是否开启 ts sourcemap
-    less: true, // 是否开启 less sourcemap
+  ts: true, // 是否开启 ts sourcemap
+  less: true, // 是否开启 less sourcemap
 };
 // options
 const srcOptions = { base: src };
@@ -29,23 +29,23 @@ const mpNpmOptions = { npmDirname: 'miniprogram_npm' };
 
 // 文件匹配路径
 const globs = {
-    ts: `${src}/**/*.ts`, // 匹配 ts 文件
-    js: `${src}/**/*.js`, // 匹配 js 文件
-    json: `${src}/**/*.json`, // 匹配 json 文件
-    less: `${src}/**/*.less`, // 匹配 less 文件
-    wxss: `${src}/**/*.wxss`, // 匹配 wxss 文件
-    // image: `${src}/**/*.{png,jpg,jpeg,gif,svg}`, // 匹配 image 文件
+  ts: `${src}/**/*.ts`, // 匹配 ts 文件
+  js: `${src}/**/*.js`, // 匹配 js 文件
+  json: `${src}/**/*.json`, // 匹配 json 文件
+  less: `${src}/**/*.less`, // 匹配 less 文件
+  wxss: `${src}/**/*.wxss`, // 匹配 wxss 文件
+  // image: `${src}/**/*.{png,jpg,jpeg,gif,svg}`, // 匹配 image 文件
 };
 globs.copy = [`${src}/**`,
-    `!${globs.ts}`, `!${globs.js}`, `!${globs.json}`,
-    `!${globs.less}`, `!${globs.wxss}`,
-    // `!${globs.image}`
+  `!${globs.ts}`, `!${globs.js}`, `!${globs.json}`,
+  `!${globs.less}`, `!${globs.wxss}`,
+  // `!${globs.image}`
 ]; // 匹配需要拷贝的文件
 
 // 包装 gulp.lastRun, 引入文件 ctime 作为文件变动判断另一标准
 // https://github.com/gulpjs/vinyl-fs/issues/226
 const since = task => (
-    file => (gulp.lastRun(task) > file.stat.ctime ? gulp.lastRun(task) : 0)
+  file => (gulp.lastRun(task) > file.stat.ctime ? gulp.lastRun(task) : 0)
 );
 
 /** `gulp clear`
@@ -62,148 +62,148 @@ const clearCache = () => cache.clearAll();
  * 清理
  * */
 const copy = () => gulp.src(
-    globs.copy,
-    { ...srcOptions, since: since(copy) },
+  globs.copy,
+  { ...srcOptions, since: since(copy) },
 )
-    .pipe(changed(dist)) // 过滤掉未改变的文件
-    .pipe(gulp.dest(dist));
+  .pipe(changed(dist)) // 过滤掉未改变的文件
+  .pipe(gulp.dest(dist));
 
 /** `gulp ts`
  * 编译ts
  * */
 const tsProject = gulpTs.createProject(resolve('tsconfig.json'));
 const ts = () => gulp.src(
-    globs.ts,
-    srcOptions,
+  globs.ts,
+  srcOptions,
 )
-    .pipe(gulpif(sourcemap.ts, sourcemaps.init()))
-    .pipe(tsProject()) // 编译ts
-    .pipe(mpNpm(mpNpmOptions)) // 分析依赖
-    .pipe(gulpif(sourcemap.ts, sourcemaps.write('.')))
-    .pipe(gulp.dest(dist));
+  .pipe(gulpif(sourcemap.ts, sourcemaps.init()))
+  .pipe(tsProject()) // 编译ts
+  .pipe(mpNpm(mpNpmOptions)) // 分析依赖
+  .pipe(gulpif(sourcemap.ts, sourcemaps.write('.')))
+  .pipe(gulp.dest(dist));
 
 /** `gulp js`
  * 解析js
  * */
 const js = () => gulp.src(
-    globs.js,
-    { ...srcOptions, since: since(js) },
+  globs.js,
+  { ...srcOptions, since: since(js) },
 )
-    .pipe(mpNpm(mpNpmOptions)) // 分析依赖
-    .pipe(gulp.dest(dist));
+  .pipe(mpNpm(mpNpmOptions)) // 分析依赖
+  .pipe(gulp.dest(dist));
 
 /** `gulp json`
  * 解析json
  * */
 const json = () => gulp.src(
-    globs.json,
-    { ...srcOptions, since: since(json) },
+  globs.json,
+  { ...srcOptions, since: since(json) },
 )
-    .pipe(mpNpm(mpNpmOptions)) // 分析依赖
-    .pipe(gulp.dest(dist));
+  .pipe(mpNpm(mpNpmOptions)) // 分析依赖
+  .pipe(gulp.dest(dist));
 
 /** `gulp less`
  * 编译less
  * */
 const less = () => gulp.src(
-    globs.less,
-    { ...srcOptions, since: since(less) },
+  globs.less,
+  { ...srcOptions, since: since(less) },
 )
-    .pipe(gulpif(sourcemap.less, sourcemaps.init()))
-    .pipe(gulpLess()) // 编译less
-    .pipe(rename({ extname: '.wxss' }))
-    .pipe(mpNpm(mpNpmOptions)) // 分析依赖
-    .pipe(gulpif(sourcemap.less, sourcemaps.write('.')))
-    .pipe(gulp.dest(dist));
+  .pipe(gulpif(sourcemap.less, sourcemaps.init()))
+  .pipe(gulpLess()) // 编译less
+  .pipe(rename({ extname: '.wxss' }))
+  .pipe(mpNpm(mpNpmOptions)) // 分析依赖
+  .pipe(gulpif(sourcemap.less, sourcemaps.write('.')))
+  .pipe(gulp.dest(dist));
 
 /** `gulp wxss`
  * 解析wxss
  * */
 const wxss = () => gulp.src(
-    globs.wxss,
-    { ...srcOptions, since: since(wxss) },
+  globs.wxss,
+  { ...srcOptions, since: since(wxss) },
 )
-    .pipe(mpNpm(mpNpmOptions)) // 分析依赖
-    .pipe(gulp.dest(dist));
+  .pipe(mpNpm(mpNpmOptions)) // 分析依赖
+  .pipe(gulp.dest(dist));
 
 /** `gulp image`
  * 压缩图片
  * */
 // const image = () => gulp.src(
-//     globs.image,
-//     { ...srcOptions, since: since(image) },
+//   globs.image,
+//   { ...srcOptions, since: since(image) },
 // )
-//     .pipe(cache(gulpImage()))
-//     .pipe(gulp.dest(dist));
+//   .pipe(cache(gulpImage()))
+//   .pipe(gulp.dest(dist));
 
 // 不清理 dist 的构建
 const _build = gulp.parallel(
-    copy,
-    ts,
-    js,
-    json,
-    less,
-    wxss,
-    image,
+  copy,
+  ts,
+  js,
+  json,
+  less,
+  wxss,
+  // image,
 );
 
 // 将 miniprogramRoot 配置修改为 dist 路径
 const config = async () => {
-    const projectFile = resolve('project.config.json');
-    const project = jsonfile.readFileSync(projectFile, { throws: false });
-    if (project) {
-        project.miniprogramRoot = path.relative(path.dirname(projectFile), dist);
-        jsonfile.writeFileSync(resolve('project.config.json'), project, { spaces: 2 });
-        project.miniprogramRoot = '/';
-        jsonfile.writeFileSync(resolve(dist, 'project.config.json'), project, { spaces: 2 });
-    }
+  const projectFile = resolve('project.config.json');
+  const project = jsonfile.readFileSync(projectFile, { throws: false });
+  if (project) {
+    project.miniprogramRoot = path.relative(path.dirname(projectFile), dist);
+    jsonfile.writeFileSync(resolve('project.config.json'), project, { spaces: 2 });
+    project.miniprogramRoot = '/';
+    jsonfile.writeFileSync(resolve(dist, 'project.config.json'), project, { spaces: 2 });
+  }
 };
 
 /** `gulp build`
  * 构建
  * */
 const build = gulp.series(
-    gulp.parallel(
-        clear,
-        // clearCache
-    ),
-    _build,
-    config,
+  gulp.parallel(
+      clear,
+      // clearCache
+  ),
+  _build,
+  config,
 );
 
 /** `gulp watch`
  * 监听
  * */
 const watch = () => {
-    gulp.watch(globs.copy, watchOptions, copy);
-    gulp.watch(globs.ts, watchOptions, ts);
-    gulp.watch(globs.js, watchOptions, js);
-    gulp.watch(globs.json, watchOptions, json);
-    gulp.watch(globs.less, watchOptions, less);
-    gulp.watch(globs.wxss, watchOptions, wxss);
-    // gulp.watch(globs.image, watchOptions, image);
+  gulp.watch(globs.copy, watchOptions, copy);
+  gulp.watch(globs.ts, watchOptions, ts);
+  gulp.watch(globs.js, watchOptions, js);
+  gulp.watch(globs.json, watchOptions, json);
+  gulp.watch(globs.less, watchOptions, less);
+  gulp.watch(globs.wxss, watchOptions, wxss);
+  // gulp.watch(globs.image, watchOptions, image);
 };
 
 /** `gulp` or `gulp dev`
  * 构建并监听
  * */
 const dev = gulp.series(
-    clear,
-    _build,
-    config,
-    watch,
+  clear,
+  _build,
+  config,
+  watch,
 );
 
 // `gulp --tasks` list tasks
 module.exports = {
-    clear,
-    clearCache,
-    copy,
-    ts,
-    less,
-    // image,
-    build,
-    watch,
-    dev,
-    default: dev,
+  clear,
+  clearCache,
+  copy,
+  ts,
+  less,
+  // image,
+  build,
+  watch,
+  dev,
+  default: dev,
 };
--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,12 +1,15 @@
 module.exports = {
-    extends: [ ],
-    globals: {
-        'wx': false,
-        'App': false,
-        'getApp': false,
-        'Page': false,
-        'getCurrentPages': false,
-        'Component': false,
-        'Behavior': false,
-    },
+  extends: [ ],
+  globals: {
+    'wx': false,
+    'App': false,
+    'getApp': false,
+    'Page': false,
+    'getCurrentPages': false,
+    'Component': false,
+    'Behavior': false,
+  },
+  rules: {
+    indent: ['error', 2],
+  },
 };
